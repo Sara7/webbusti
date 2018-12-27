a433f9c517f2e1367e9ad15cb43b7a2aa0c0243f
Sara <sara.pupi@iamdev.it>
2018-12-27 16:16:53 +0100

    Hashed product api

--- API/product.php
+++ API/product.php
@@ -7,10 +7,23 @@
 
     switch($action) {
         case "list":
-            if($_GET["category_code"] != null) {
-                $result = $pdo -> select("product", ["product_category*" => $category_code]);
+            if($_GET["product_category"] != null) {
+                $product_category = $_GET["product_category"];
+                $result = $pdo -> select("product", ["product_category*" => $product_category], ["product_category" => "ASC"]);
             } else {
                 $result = $pdo -> select("product", [], ["product_category" => "ASC", "product_name_default" => "DESC"]);
+            }
+            if($_GET["hashed"] != null && $_GET["hashed"] == "true") {
+                $structured_products = [];
+                foreach($result as $res) {
+                    $product_category = $res["product_category"];
+                    $product_category_root = explode("_", $product_category)[0];
+                    if(!array_key_exists($product_category_root, $structured_products)) {
+                        $structured_products[$product_category_root] = [];
+                    }
+                    $structured_products[$product_category_root][] = $res;   
+                }
+                $result = $structured_products;
             }
             break;
     }
